35ff69e497986626f310c92be68095fdeb1b73c9
Jindal Mistry <mistryjindal@gmail.com>
2024-03-07 15:58:43 +0530

    Responsive design related changes for sidebar and Landing page

--- chess-client/src/app/component/sidebar/sidebar.js
+++ chess-client/src/app/component/sidebar/sidebar.js
@@ -1,55 +1,62 @@
-import React from "react";
+import {createPortal} from 'react-dom'
+import React, { useEffect, useRef, useState } from "react";
 import "./sidebar.css";
 
-export default function Sidebar() {
+export default function Sidebar({toggle}) {
+  const sidebarItem = [
+    "Play online",
+    "Play against AI",
+    "VS Friend",
+    "Achievements",
+    "Results",
+    "Get Better",
+    "Help & Support",
+    "Log out"
+  ];
+  const sidebarRef = useRef();
+  const [checked, setChecked] = useState(true);
+
+  useEffect(() => {
+    if(toggle === true) {setChecked(!checked)}
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toggle])
+
+  useEffect(() => {
+    if(sidebarRef && sidebarRef.current) {
+      window.addEventListener('mousedown', (e) => { 
+        if(!sidebarRef.current.contains(e.target)) {
+          setChecked(true);
+        }
+      })
+    }
+  },[sidebarRef])
   return (
-    <div className="sidebar-wrapper">
-      <div>
-        <header className="sidebar-header">
-          <div className="sidebar-logo">
-          </div>
-        </header>
-        <main className="sidebar-main">
-          <ul>
-            <li className="sidebar-item">
-              <div className="sidebar-item-logo">
+    <>
+      <div id="nav-bar" ref={sidebarRef}>
+        <input id="nav-toggle" type="checkbox" checked={checked} onChange={(e) => {setChecked(e.target.checked)}}/>
+        <div id="nav-header">
+          <span id="nav-title">KingsBane</span>
+          <label for="nav-toggle">
+            <i class="fas fa-camera"></i>
+          </label>
+        </div>
+        <div id="nav-content">
+          {sidebarItem.map((item, index) => {
+            return (
+              <div class="nav-button">
+                <i class="fas fa-camera"></i>
+                <span>{item}</span>
               </div>
-              <span>Play online</span>
-            </li>
-            <li className="sidebar-item">
-              <div className="sidebar-item-logo"></div>
-              <span>Play against AI</span>
-            </li>
-            <li className="sidebar-item">
-              <div className="sidebar-item-logo"></div>
-              <span>VS Friend</span>
-            </li>
-            <li className="sidebar-item">
-              <div className="sidebar-item-logo"></div>
-              <span>Achievements</span>
-            </li>
-            <li className="sidebar-item">
-              <div className="sidebar-item-logo"></div>
-              <span>Results</span>
-            </li>
-            <li className="sidebar-item">
-              <div className="sidebar-item-logo"></div>
-              <span>Get Better</span>
-            </li>
-          </ul>
-        </main>
+            );
+          })}
+        </div>
       </div>
-      <footer className="sidebar-footer">
-        <div className="sidebar-item">
-          <div className="sidebar-item-logo"></div>
-          <span>Help & Support</span>
-        </div>
-        <div className="sidebar-item">
-          <div className="sidebar-item-logo">
-          </div>
-          <span>Log out</span>
-        </div>
-      </footer>
-    </div>
+      {!checked ? 
+        createPortal(
+          <div id="layer"></div>,
+          document.body
+        )
+      : null}
+    </>
   );
 }
